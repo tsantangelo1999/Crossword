62462a282ac9824144af0aab7d1b79d0bbd6a3cd
ts159 <tommys2056@gmail.com>
2017-05-18 14:30:30 -0400

    still confuzzle

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 import java.io.*;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main
 {
@@ -13,22 +14,41 @@ public class Main
         Board board = new Board();
         File file = new File("words.txt");
         Scanner sc = new Scanner(file);
-        ArrayList<String> across = new ArrayList<>();
-        ArrayList<String> down = new ArrayList<>();
+        ArrayList<Word> across = new ArrayList<>();
+        ArrayList<Word> down = new ArrayList<>();
         boolean acrossDone = false;
         while(sc.hasNextLine())
         {
             if(!acrossDone)
-                across.add(sc.nextLine());
+                across.add(new Word(sc.nextLine()));
             else
-                down.add(sc.nextLine());
-            if(across.get(across.size() - 1).equalsIgnoreCase(""))
+                down.add(new Word(sc.nextLine()));
+            if(across.get(across.size() - 1).word.equalsIgnoreCase(""))
             {
                 acrossDone = true;
                 across.remove(across.size() - 1);
             }
         }
+        for(Word w : across)
+        {
+            w.countConnections(down);
+        }
+        for(Word w : down)
+        {
+            w.countConnections(across);
+        }
+        Collections.sort(across);
+        Collections.sort(down);
         System.out.println(across);
         System.out.println(down);
+        ArrayList<ArrayList<Character>> letters = new ArrayList<>();
+
+        while(across.size() > 0 || down.size() > 0)
+        {
+            if(across.get(0).connections <= down.get(0).connections)
+            {
+
+            }
+        }
     }
 }
---
+++ src/com/company/Word.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Word implements Comparable<Word>
+{
+    public final String word;
+
+    public int connections;
+
+    public Word(String w)
+    {
+        word = w;
+        connections = 0;
+    }
+
+    public int compareTo(Word word1)
+    {
+        if(connections < word1.connections)
+            return -1;
+        else if(connections > word1.connections)
+            return 1;
+        else
+            return 0;
+    }
+
+    public void countConnections(ArrayList<Word> words)
+    {
+        top: for(Word w : words)
+        {
+            char[] chars = w.word.toCharArray();
+            for(char c : chars)
+            {
+                if(word.contains(String.valueOf(c)))
+                {
+                    connections++;
+                    continue top;
+                }
+            }
+        }
+    }
+
+    public String toString()
+    {
+        return word + " - Connections: " + connections;
+    }
+}
